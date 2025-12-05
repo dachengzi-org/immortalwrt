23e8a73fa698bcb21acd15dfb7c77220d0f5a531
Ethan Zuo <yuxuan.zuo@outlook.com>
2025-12-05 23:44:15 +0800

    build: introduce nonshared repository configuration
    Introduce a new menuconfig option VERSION_NONSHARED_REPO, which
    allows users to set a separate URL specifically for target specific
    (nonshared) packages repository.
    
    We use a new %N placeholder to represent this new repository URL
    for the core and kmods feeds. The base feed and other enabled feeds
    continue to use the standard VERSION_REPO URL.
    
    This flexibility is useful for scenarios where target specific
    binaries and kernel modules are hosted on a different mirror or path.
    
    Signed-off-by: Ethan Zuo <yuxuan.zuo@outlook.com>

--- include/version.mk
+++ include/version.mk
@@ -13,6 +13,7 @@ PKG_CONFIG_DEPENDS += \
 	CONFIG_VERSION_NUMBER \
 	CONFIG_VERSION_CODE \
 	CONFIG_VERSION_REPO \
+	CONFIG_VERSION_NONSHARED_REPO \
 	CONFIG_VERSION_DIST \
 	CONFIG_VERSION_MANUFACTURER \
 	CONFIG_VERSION_MANUFACTURER_URL \
@@ -30,6 +31,9 @@ VERSION_CODE:=$(if $(VERSION_CODE),$(VERSION_CODE),$(REVISION))
 
 VERSION_REPO:=$(call qstrip,$(CONFIG_VERSION_REPO))
 VERSION_REPO:=$(if $(VERSION_REPO),$(VERSION_REPO),https://downloads.immortalwrt.org/releases/21.02-SNAPSHOT)
+
+VERSION_NONSHARED_REPO:=$(call qstrip,$(CONFIG_VERSION_NONSHARED_REPO))
+VERSION_NONSHARED_REPO:=$(if $(VERSION_NONSHARED_REPO),$(VERSION_NONSHARED_REPO),https://downloads.immortalwrt.org/releases/21.02-SNAPSHOT)
 
 VERSION_DIST:=$(call qstrip,$(CONFIG_VERSION_DIST))
 VERSION_DIST:=$(if $(VERSION_DIST),$(VERSION_DIST),ImmortalWrt)
@@ -87,6 +91,7 @@ endef
 #'
 
 VERSION_SED_SCRIPT:=$(SED) 's,%U,$(call sed_escape,$(VERSION_REPO)),g' \
+	-e 's,%N,$(call sed_escape,$(VERSION_NONSHARED_REPO)),g' \
 	-e 's,%V,$(call sed_escape,$(VERSION_NUMBER)),g' \
 	-e 's,%v,\L$(call sed_escape,$(subst $(space),_,$(VERSION_NUMBER))),g' \
 	-e 's,%C,$(call sed_escape,$(VERSION_CODE)),g' \
